74290e824672f0c919b421cee4dcda73b1aaf47c
Alize Nguyen <alizenguyen@gmail.com>
2018-04-28 13:37:12 -0500

    BookSearch component route added

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,68 +1,12 @@
 import React, { Component } from 'react'
 
 class BookSearch extends React.Component {
-  state = {
-    books: [],
-  }
-
-  componentDidMount() {
-    this.handleBooksRetreival();
-  }
-
-  handleBooksRetreival = () => {
-    BooksAPI.getAll()
-      .then((books) => {
-        this.setState(() => ({
-          books
-        }))
-        console.log(books);
-    })
-  }
-
-  handleShelfChange = (book, shelf) => {
-    BooksAPI.update(book, shelf)
-      .then(() => {
-        this.handleBooksRetreival();
-      })
-  }
+  
 
   render() {
-    return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div>
-            <SavedBooks 
-              books={this.state.books}
-              onChange={this.handleShelfChange}
-              />
-            <button className="add-button">
-            </button>
-          </div>
-        )}
-      </div>
-    )
-  }
+      return (
+        <div>Hello</div>
+      )
+    }
 }
-
 export default BookSearch
